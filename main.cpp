66568c98420cf2f821503ff4d2bf1f9893ff3c4c
jonatasricosti <jonatas-ricosti@hotmail.com>
2024-02-09 23:34:24 -0300

    adicionei uma função que desenha imagem na tela

--- main.cpp
+++ main.cpp
@@ -8,6 +8,31 @@ const int screen_width = 640;
 const int screen_height = 480;
 const int screen_bpp = 32;
 
+
+// use essa função pra desenhar uma imagem na tela
+void DrawImage(int x, int y, SDL_Surface *image)
+{
+    SDL_Rect mover;
+    mover.x = x;
+    mover.y = y;
+
+    SDL_BlitSurface(image, NULL, tela, &mover);
+}
+
+
+// use essa função pra carregar arquivos
+// nota: essa função só deve ser chamada no começo do programa
+void LoadFiles()
+{
+}
+
+
+// use essa função pra fechar arquivos
+// nota: essa função só deve ser chamada no final do programa
+void CloseFiles()
+{
+}
+
 // para o framerate
 Uint32 start = 0;
 const int fps = 30;
